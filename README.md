959391b9691e71b859e4ca8edb5717bc30b39ea3
Florian Behrens <florian.behrens@rocketmail.com>
2017-11-12 03:59:34 +0100

    Add some documentation

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # programming-grader
+A project to collect solutions to programming exercises from
+students through a common web interface.
+
+In the future, docker based automatic grading may be added to
+the project.
+
+## Usage
+
+After you have installed and configured this tool (see below), users will be
+able to submit solutions. The solutions will be saved in
+the directory that you specify during configuration, according
+to the following scheme: `t11/123123_356348/2017-11-12_02:26:25/ex4/`
+where `t11` means that the files were submitted for tutor 11, 
+`123123_356348` are the matriculation numbers of the students,
+`2017-11-12_02:26:25` is the time of submission and `ex4` means
+that the solutions were submitted for exercise 4.
+
+The files will also be sent as a zip archive to the email address that is specified
+for the relevant tutor.
+
+## Installation
+The grader application is meant to be served by a webserver like
+Apache that is configured to execute php scripts.
+
+1. Clone the repository
+1. `cd backend`
+1. Install the dependencies of the backend: `bower install` (this
+requires the `bower` package manager)
+1. `cd ../frontend`
+1. Install the dependencies of the frontend: `npm install` (this
+requires the node package manager `npm`)
+1. Build the frontend files: `node_modules/.bin/webpack`
+1. Have the server of your choice serve the `wwwroot` directory
+    - make sure php scripts are executed
+    - the webserver needs to have write access to the
+    directory to which the submitted files are uploaded,
+    which can be configured (see below)
+1. Configure the project as described below.
+
+## Configuration
+
+1. Copy `sample.config.php` to `config.php`
+1. Adapt `config.php` according to the comments it contains.
--- sample.config.php
+++ sample.config.php
@@ -1,18 +1,33 @@
 <?php
 
 return [
+    // This is used to notify the tutors about new submissions.
     'smtp' => [
         'host' => 'smtp.google.com',
         'user' => 'smtp_user_name',
         'pass' => 'a8s7d6f8sd7f',
         'from' => 'no-reply@somehost.com',
     ],
+    // After this many submissions have been uploaded, no more
+    // submissions will be accepted (to prevent DoS attacks etc.)
     'maxSubmissions' => 1000,
-    'uploadDirectory' => '/some/absolute/path/',  // This should be outside the wwwroot, and the webserver needs to have write access.
+    // The directory to which the submissions will be uploaded.
+    // Make sure that the web server serving the application has
+    // write access to that directory. Also, make sure
+    // that the upload directory is _not_ contained in the wwwroot
+    // directory! Otherwise, submissions will be available for
+    // anyone to download.
+    // Paths can be specified absolutely or relative to the wwwroot.
+    // The path must end in /
+    'uploadDirectory' => '../submissions/',
+    // List of tutorials.
     'tutorials' => [
         [
+            // The id must be unique.
             'id' => 11,
+            // Only the description is made public.
             'description' => '11 - Mittwoch 08:30 - Florian Behrens',
+            // Name and mail address are used only to notify the tutors.
             'tutor' => 'Florian Behrens',
             'mail' => 'florian.behrens@rocketmail.com',
         ],
@@ -23,17 +38,34 @@ return [
             'mail' => 'radu@somemailservice.com',
         ],
     ],
+    // The exercises for which submissions are possible.
     'exercises' => [
         [
+            // Id must be unique.
             'id' => 4,
+            // The name of the exercise as displayed to the user.
             'name' => 'Blatt 3 - Aufgabe 4',
-            'allowAdditionalFiles' => true,
+            // Specify what files can be uploaded.
             'files' => [
                 [
+                    // Name of the file
                     'name' => 'Main.java',
+                    // Determines whether the user can change this file.
+                    // For example, you might have code that supplies helper
+                    // functionality, which you don't want every student to upload.
+                    // Then you can mark that file as given, and users will see
+                    // that that file is supplied to them.
                     'isGiven' => false,
+                    // When displaying the file, what mode should the CodeMirror
+                    // editor use for displaying that code? (basically, that's
+                    // the language of the file)
                     'mode' => 'text/x-java',
+                    // The contents of the file (the user can of course adapt this).
                     'code' => '// Hier Code für Main.java einfügen.',
+                ],
+                [
+                    'name' => 'SimpleIO.java',
+                    'isGiven' => true,
                 ]
             ]
         ]
